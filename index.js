fefae6ff299b305a82134faf3c2fc63b3b5dc2a7
NeoD1m <57021314+NeoD1m@users.noreply.github.com>
2021-11-10 16:05:56 +0300

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+
+let history = [];
+let clients = [];
+
+const colors = ['blue', 'red', 'green', 'yellow'];
+
+const httpServer = http.createServer(() => { });
+httpServer.listen(1337, () => { });
+
+const wsServer = new WebSocketServer({ httpServer });
+
+wsServer.on('request', request => {
+    const connection = request.accept(null, request.origin);
+
+    console.log(`${new Date()} Connection from origin ${request.origin}.`);
+
+    const index = clients.push(connection) - 1;
+    let userName = undefined;
+    let userColor = undefined;
+
+    console.log(`${new Date()} Connection accepted.`);
+
+    if (history.length > 0)
+        connection.sendUTF(JSON.stringify({ type: 'history', data: history }));
+
+    connection.on('message', message => {
+        if (message.type === 'utf8') { // accept only text
+            // first message sent by user is their name
+            if (!userName) {
+                userName = message.utf8Data;
+                userColor = colors[Math.floor(Math.random() * 4)];
+
+                connection.sendUTF(JSON.stringify({ type: 'color', data: userColor }));
+                console.log(`${new Date()} User is known as: ${userName} with ${userColor} color.`);
+            } else {
+                console.log(`${new Date()} Received Message from ${userName}: ${message.utf8Data}`);
+
+                const msg = {
+                    time: (new Date()).getTime(),
+                    text: message.utf8Data,
+                    author: userName,
+                    color: userColor
+                };
+
+                history.push(msg);
+                history = history.slice(-100);
+                // broadcast message to all connected clients
+                const json = JSON.stringify({ type: 'message', data: msg });
+                clients.forEach(c => c.sendUTF(json));
+            }
+        }
+        connection.on('close', c => {
+            if (userName !== false && userColor !== false) {
+                console.log(`${new Date()} Peer ${connection.remoteAddress} disconnected.`);
+                clients.splice(index, 1);
+            }
+        });
+
+    });
+});
